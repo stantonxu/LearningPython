fa01240e7135ce11c46151820ffe552e5480a672
Stanton Xu <xjiefeng@gmail.com>
2018-01-12 00:23:36 -0500

    ready to run

--- PythonProjects/NoSQL/setup.py
+++ PythonProjects/NoSQL/setup.py
@@ -5,6 +5,7 @@ import socket
 HOST = 'localhost'
 PORT = 50505
 SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 stats = {
     'PUT': {'success': 0, 'error': 0},
     'GET': {'success': 0, 'error': 0},
@@ -17,43 +18,15 @@ stats = {
 }
 
 COMMAND_HANDLERS = {
-    'PUT': handle_put,
-    'GET': handle_get,
-    'GETLIST': handle_getlist,
-    'PUTLIST': handle_putlist,
-    'INCREMENT': handle_increment,
-    'APPEND': handle_append,
-    'DELETE': handle_delete,
-    'STATS': handle_stats
+    'PUT': 'handle_put',
+    'GET': 'handle_get',
+    'GETLIST': 'handle_getlist',
+    'PUTLIST': 'handle_putlist',
+    'INCREMENT': 'handle_increment',
+    'APPEND': 'handle_append',
+    'DELETE': 'handle_delete',
+    'STATS': 'handle_stats'
 }
-
-def main():
-    '''Main entry point of script'''
-    SOCKET.bind((HOST, PORT))
-    SOCKET.listen(1)
-    while 1:
-        connection, address = SOCKET.accept()
-        print("New connection from [{}]".format(address))
-        data = connection.recv(4096).decode()
-        command, key, value = parse_message(data)
-        if command == 'STATS':
-            response = handle_stats()
-        elif command in (
-            'GET',
-            'GETLIST',
-            'INCREMENT',
-            'DELETE'
-        ):
-            response = COMMAND_HANDLERS[command](key, value)
-        else:
-            response = (False, 'Unknown command type [{}]'.format(command))
-        update_stats(command, response[0])
-        connection.sendall('{};{}'.format(response[0], response[1]))
-        connection.close()
-
-if __name__ == '__main__':
-    main()
-
 
 def parse_message(data):
     '''Return a tuple containing the command, the key, and (optionally) the value cast to the approriate type.'''
@@ -135,3 +108,32 @@ def handle_delete(key):
 def handle_stats():
     '''Retuen a tuple containing Tre and the contents of the STATS dict.'''
     return (True, str(STATS))
+
+
+
+def main():
+    '''Main entry point of script'''
+    SOCKET.bind((HOST, PORT))
+    SOCKET.listen(1)
+    while 1:
+        connection, address = SOCKET.accept()
+        print("New connection from [{}]".format(address))
+        data = connection.recv(4096).decode()
+        command, key, value = parse_message(data)
+        if command == 'STATS':
+            response = handle_stats()
+        elif command in (
+            'GET',
+            'GETLIST',
+            'INCREMENT',
+            'DELETE'
+        ):
+            response = COMMAND_HANDLERS[command](key, value)
+        else:
+            response = (False, 'Unknown command type [{}]'.format(command))
+        update_stats(command, response[0])
+        connection.sendall('{};{}'.format(response[0], response[1]))
+        connection.close()
+
+if __name__ == '__main__':
+    main()
