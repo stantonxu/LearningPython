f299b1a6d44c9bde2cbf281eb483a4561de882d3
Stanton Xu <xjiefeng@gmail.com>
2018-01-11 22:04:09 -0500

    Updated on plot, working version

--- LeetCode/plot.py
+++ LeetCode/plot.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 def f(x):
     return 100 / x + x
 
-x1 = np.arange(1, 100, 1)
+x1 = np.arange(1, 100, 0.01)
 
 plt.figure(1)
-plt.plot(x1, f(x1))
+plt.plot(x1, f(x1), 'bo')
+plt.show()
